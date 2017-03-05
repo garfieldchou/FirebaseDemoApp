673681d8295b673b7e9557ac7de9d9afc039c865
shaudai <shaudai0402@hotmail.com>
2017-03-05 13:15:39 +0800

    Create a database reference to save an object to firebase database
    1. Create a database reference
    2. Use a Map to save key-value pairs
    3. Push the Map via the database reference with push().setValue() API and
    setup corresponding CompletionListener

--- app/src/main/java/com/garfieldchou/firebasedemoapp/MainActivity.java
+++ app/src/main/java/com/garfieldchou/firebasedemoapp/MainActivity.java
@@ -2,6 +2,14 @@ package com.garfieldchou.firebasedemoapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +17,27 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        DatabaseReference dbref = FirebaseDatabase.getInstance().getReference();
+
+        Map<String, String> values = new HashMap<>();
+
+        values.put("name", "rob");
+
+        dbref.push().setValue(values, new DatabaseReference.CompletionListener() {
+            @Override
+            public void onComplete(DatabaseError databaseError, DatabaseReference databaseReference) {
+
+                if (databaseError == null) {
+
+                    Log.i("Info", "Save successful");
+
+                } else {
+
+                    Log.i("Info", "Save failed");
+
+                }
+            }
+        });
     }
 }
